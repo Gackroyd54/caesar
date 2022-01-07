02d42341fae0a54896bcd876856329ac771edcaf
Gabriel Costa Fernandes Pereira <83623893+Gackroyd54@users.noreply.github.com>
2022-01-06 23:58:55 -0300

    Add files via upload

---
+++ caesar.js
@@ -0,0 +1,10 @@
+//Pegando os input de ichave e ijanel:
+var chave = document.getElementById("ichave")//chave
+var palavra = document.getElementById("ipalavra")//palavra
+var res = document.getElementById("resultado")//resultado(A cifra)
+
+function codificar(){
+    for (letrap of palavra.value) {
+        console.log(letrap)
+    }
+}
